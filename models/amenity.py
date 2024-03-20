6d726707741f87d036c8603cd6d43dd101f486f9
ElvisOmbiro <elvisombiro99@gmail.com>
2024-03-20 17:44:33 +0300

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, Table, String, ForeignKey
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True, nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False))
 
 
 class Amenity(BaseModel, Base):
-    """Represent an Amenity for a MySQL database.
-
-    Attributes:
-        name: The Amenity name
-        place_amenities (relationship): The Place - Amenity relationship.
-
-    """
-
-    __tablename__ = "amenities"
-
+    """Representation of Amenity """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary=place_amenity)
+        __tablename__ = 'amenities'
+        name = Column(String(128),
+                      nullable=False)
     else:
-        name = ''
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
