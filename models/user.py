4d7fb970ab1802e33b13b4b9eb30257ae4e4da33
ElvisOmbiro <elvisombiro99@gmail.com>
2024-03-20 15:50:33 +0300

    new adaptation

--- models/user.py
+++ models/user.py
@@ -1,47 +1,36 @@
 #!/usr/bin/python3
-""" holds class User"""
-import hashlib
-import models
+"""This is the user class"""
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """ Represent a user for a MySQL database.
+
+    Attributes:
+        __tablename__: represents the table name, users
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The user-Place relationship.
+        reviews (sqlalchemy relationship): The user-Review relationship.
+
+    """
+
+    __tablename__ = "users"
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
     else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
